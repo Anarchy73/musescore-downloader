962efb014c2e05bed4e30e1ef92e20c4bde6f6b4
Anarchy73 <kanalcraba@gmail.com>
2026-02-18 15:22:51 +0400

    Muplti page support

--- svg2pdf.py
+++ svg2pdf.py
@@ -1,6 +1,24 @@
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
 
-drawing = svg2rlg("aboba.svg")
-drawing.add(svg2rlg("aboba.svg"))
-renderPDF.drawToFile(drawing, "output_file.pdf")
+import os
+
+def create_pdf_from_svgs(svg_files, output_file):
+    c = canvas.Canvas(output_file, pagesize=A4)
+    for svg in svg_files:
+        drawing = svg2rlg(svg)
+        if drawing:
+            drawing.scale(1, 1)
+            renderPDF.draw(drawing, c, 0, 0)
+            if svg != svg_files[-1]:
+                c.showPage()
+    c.save()
+
+svg_files = [
+    "aboba.svg",
+    "aboba2.svg",
+]
+
+create_pdf_from_svgs(svg_files, "output.pdf")
