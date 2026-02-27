270f3f7771f3b82df26b03b7d04cb1ef6ea8eb7e
Anarchy73 <kanalcraba@gmail.com>
2026-02-28 01:29:04 +0400

    Feat: pdf from svgs

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from curl_cffi import requests
 from fastapi import FastAPI
 from typing import List
-
+from svg2pdf import create_pdf_from_svgs
 app = FastAPI()
 
 @app.post("/pdf/")
 async def makepdf(srcs: List[str]):
-  print(srcs)
-  for i in range(len(srcs)):
-    response = requests.get(srcs[i], impersonate="chrome110")
-    if response.status_code == 200:
-        with open(f'source_{i}.svg', 'wb') as f:
-            f.write(response.content)
-        print("Файл успешно скачан!")
-    else:
-        print(f"Ошибка: {response.text[:500]}")
+    print(srcs)
+    files = []
+    for i in range(len(srcs)):
+        filename = f'source_{i}.svg'
+        response = requests.get(srcs[i], impersonate="chrome110")
+        if response.status_code == 200:
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            print("Файл успешно скачан!")
+        else:
+            print(f"Ошибка: {response.text[:500]}")
+        files.append(filename)
+    create_pdf_from_svgs(files, "output.pdf")
